97c482e297d36c04692f0b0e3171fe2a1688dcf8
realfakeuser <tim.niewerth@tobit.software>
2018-08-30 15:29:42 +0200

    exception handling checks added

--- src/constants/text.js
+++ src/constants/text.js
@@ -1,2 +1,2 @@
 export const HEADLINE = 'Site-finder';
-export const DESCRIPTION = 'In diesem Tab kannst Du Sites nach Inhalten durchsuchen';
+export const DESCRIPTION = 'In diesem Tab kannst Du Sites finden und Deine persönliche Site hinzufügen';
